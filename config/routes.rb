0c43953563a30c895fcfc09a379728083780d4aa
farstreets <rp.verstraten@gmail.com>
2018-06-05 10:58:06 +0200

    cf4.1 products page scaffold added

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   get 'simple_pages/index'
   get 'simple_pages/about'
   get 'simple_pages/contact'
+  # root 'products#index'
   root 'simple_pages#index'
+  resources :products
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
 
