bb64feca24bab9373f55b0d036fa2aaf167cd76a
farstreets <rp.verstraten@gmail.com>
2018-06-20 21:44:53 +0200

    Mail on Heroku retry

--- app/controllers/simple_pages_controller.rb
+++ app/controllers/simple_pages_controller.rb
@@ -14,7 +14,7 @@ class SimplePagesController < ApplicationController
     @email = params[:email]
     @message = params[:message]
     # ActionMailer::Base.mail(from: @email,
-    # to: 'your-email@example.com',
+    # to: 'rp.verstraten@gmail.com',
     # subject: "A new contact form message from #{@name}",
     # body: @message).deliver_now
     UserMailer.contact_form(@email, @name, @message).deliver_now
