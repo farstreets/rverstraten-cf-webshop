9d944441e40ac6ef19d43eb895a4b61cbff3fc1c
farstreets <rp.verstraten@gmail.com>
2018-06-20 22:23:43 +0200

    mail retry

--- config/environment.rb
+++ config/environment.rb
@@ -13,3 +13,13 @@ ActionMailer::Base.smtp_settings = {
   domain: 'heroku.com',
   enable_starttls_auto: true
 }
+
+# ActionMailer::Base.smtp_settings = {
+#   :user_name => ENV['SENDGRID_USERNAME'],
+#   :password => ENV['SENDGRID_PASSWORD'],
+#   :domain => 'yourdomain.com',
+#   :address => 'smtp.sendgrid.net',
+#   :port => 587,
+#   :authentication => :plain,
+#   :enable_starttls_auto => true
+# }
