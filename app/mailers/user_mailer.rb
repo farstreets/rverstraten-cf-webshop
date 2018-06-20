dd64a8ac08bc0cb77b8e7eeeeb4711893e100066
farstreets <rp.verstraten@gmail.com>
2018-06-20 21:52:13 +0200

    Mail on Heroku retry

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,8 +1,8 @@
 class UserMailer < ApplicationMailer
-  default from: "from@example.com"
+  default from: "rp.verstraten@gmail.com"
 
   def contact_form(email, name, message)
-  @message = message
+    @message = message
     mail(from: email,
          to: 'rp.verstraten@gmail.com',
          subject: "A new contact form message from #{name}")
