341b8d86d0f2dd32b9f49f840a141499fb240e2e
farstreets <rp.verstraten@gmail.com>
2018-06-27 22:14:45 +0200

    CF 5.9 Comment Create Controller Correction

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -10,21 +10,5 @@ class Comment < ApplicationRecord
   scope :rating_desc, -> { order(rating: :desc) }
   # Called by e.g. <%= @product.comments.rating_asc.first %>
   scope :rating_asc, -> { order(rating: :asc) }
-
-  def create
-    @product = Product.find(params[:product_id])
-    @comment = @product.comments.new(comment_params)
-    @comment.user = current_user
-
-    respond_to do |format|
-      if @comment.save
-        format.html { redirect_to @product, notice: 'Review was created successfully.' }
-        format.json { render :show, status: :created, location: @product }
-      else
-        format.html { redirect_to @product, alert: 'Review was not saved successfully.' }
-        format.json { render json: @comment.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
+  
 end
