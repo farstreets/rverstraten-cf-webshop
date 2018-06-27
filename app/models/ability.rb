22d1b365cf22e4c063eb4de4eb580b67f4e26946
farstreets <rp.verstraten@gmail.com>
2018-06-27 12:47:32 +0200

    cf 5.10: Admin Role & Moderating User Comments

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,7 +4,12 @@ class Ability
   def initialize(user)
 
     user ||= User.new # guest user (not logged in)
-    can :manage, User, id: user.id
+    if user.admin?
+      can :manage, :all
+    else
+      can :manage, User, id: user.id
+      can :manage, Comment, user_id: user.id
+    end
 
     # Define abilities for the passed in user here. For example:
     #
