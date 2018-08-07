7f433de7b2649d5c29486cade2ec9a4c29148c99
farstreets <rp.verstraten@gmail.com>
2018-08-07 16:42:36 +0200

    first dummy commit on new branch

--- app/assets/javascripts/channels/product.js
+++ app/assets/javascripts/channels/product.js
@@ -11,4 +11,5 @@ App.product = App.cable.subscriptions.create("ProductChannel", {
     // Called when there's incoming data on the websocket for this channel
     $(".alert.alert-info").show();
   }
+  
 });
