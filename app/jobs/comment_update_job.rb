2fc3ad6a10da52ed2f4b5b0b3890a73e44acf3d3
farstreets <rp.verstraten@gmail.com>
2018-10-22 07:57:03 +0200

    CF 6.15 Production-Ready Web App

--- app/jobs/comment_update_job.rb
+++ app/jobs/comment_update_job.rb
@@ -1,6 +1,10 @@
 class CommentUpdateJob < ApplicationJob
   queue_as :default
 
+  # This test gives warning:
+  # CommentUpdateJob add some examples to (or delete) /Users/farstreets/WebDev/3_10 Restart/rverstraten_webshop/spec/jobs/comment_update_job_spec.rb
+  #   # Not yet implemented
+  #   # ./spec/jobs/comment_update_job_spec.rb:4
   def perform(comment, current_user)
     ProductChannel.broadcast_to(comment.product_id, comment: render_comment(comment, current_user), average_rating: comment.product.average_rating)
   end
@@ -10,5 +14,5 @@ class CommentUpdateJob < ApplicationJob
     def render_comment(comment, current_user)
       CommentsController.render(partial: 'comments/comment', locals: { comment: comment, current_user: current_user })
     end
-  
+
 end
--- spec/controllers/products_controller_spec.rb
+++ spec/controllers/products_controller_spec.rb
@@ -1,3 +1,15 @@
+################################################################
+# Controller actions
+################################################################
+# Index
+# Show
+# New
+# Edit
+# Create
+# Update
+# Destroy
+################################################################
+
 require 'rails_helper'
 
 describe ProductsController, type: :controller do
@@ -7,7 +19,7 @@ describe ProductsController, type: :controller do
     @user = FactoryBot.create(:random_user)
     @product = FactoryBot.create(:random_product)
   end
-  
+
   ########################################################
   # Test: Get the products INDEX page
   ########################################################
@@ -69,6 +81,20 @@ describe ProductsController, type: :controller do
   end
 
   ########################################################
+  # Test: NEW action
+  ########################################################
+
+  describe 'GET #new ->' do
+    context 'admin ->' do
+      it "all good" do
+        sign_in @admin
+        get :new
+        expect(response).to render_template('new')
+      end
+    end
+  end
+
+  ########################################################
   # Test: Get the products EDIT page
   ########################################################
   describe 'GET #show ->' do
@@ -96,6 +122,54 @@ describe ProductsController, type: :controller do
         # expect(response).to redirect_to(root_path)
         # expect(flash[:alert]).to be_present
         # expect(flash[:alert]).to match("You are not authorized to access this page.")
+      end
+    end
+
+  end
+
+  ########################################################
+  # Test: CREATE action
+  ########################################################
+
+  # Directions from course text (6.15):
+  # test 1: what happens when a product is created with valid data
+  # test 2: what happens when a user tries to create a product,
+  # but doesn't include all of the required information.
+
+  describe "#create ->" do
+
+    context "admin + valid ->" do
+      it 'all good' do
+        sign_in @admin
+        @product_ok = FactoryBot.create(:random_product, name: "I_AM_OK" )
+        expect(response).to be_ok
+        expect(@product_ok.name).to eq("I_AM_OK")
+      end
+    end
+
+    let(:no_name_product) { FactoryBot.create(:random_product) }
+    context "admin + name missing ->" do
+      it 'fail' do
+        sign_in @admin
+        no_name_product.name = nil
+        post :create, params: { product: no_name_product.attributes }
+        expect(response).to render_template('new')
+      end
+    end
+
+  end
+
+  ########################################################
+  # Test: UPDATE action
+  ########################################################
+
+  describe "#update ->" do
+
+    context "admin + valid ->" do
+      it 'all good' do
+        sign_in @admin
+        patch :update, params: { id: @product.id, product: { :name => "new title", :description => "new description" }}
+        expect(response).to be_redirect
       end
     end
 
